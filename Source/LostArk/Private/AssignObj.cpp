1d3da77a11014ab52764c124ba6d456372d11319
micalia <60741767+micalia@users.noreply.github.com>
2024-12-01 22:35:51 +0900

    .

--- Source/LostArk/Private/AssignObj.cpp
+++ Source/LostArk/Private/AssignObj.cpp
@@ -147,3 +147,8 @@ void AAssignObj::EndOverlapCallFunc()
 		overlapCount--;
 	}
 }
+
+void AAssignObj::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
--- Source/LostArk/Public/AssignObj.h
+++ Source/LostArk/Public/AssignObj.h
@@ -64,4 +64,12 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void EndOverlapCallFunc();
 
+	UFUNCTION()
+	void OnOverlap(
+			UPrimitiveComponent* OverlappedComponent,
+			AActor* OtherActor,
+			UPrimitiveComponent* OtherComp,
+			int32 OtherBodyIndex,
+			bool bFromSweep,
+			const FHitResult& SweepResult);
 };
